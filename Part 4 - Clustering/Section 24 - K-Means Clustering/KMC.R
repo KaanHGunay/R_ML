6df11aa76cb08e28334b8f8930379dfd3b854dfa
KaanHGunay <kaangunay16@gmail.com>
2018-09-02 20:14:09 +0300

    KMC

---
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/KMC.R
@@ -0,0 +1,28 @@
+# K-Means Clustering
+
+dataset <- read.csv('Mall_Customers.csv')
+X <- dataset[, 4:5]
+
+# Using the elbow method to find the optimal of clusters
+set.seed(6)
+wcss <- vector()
+for (i in 1:10) wcss[i] <- sum(kmeans(X, i)$withinss)
+plot(1:10, wcss, type = 'b', main = 'Clusters of clients', xlab = 'Number of Clusters', ylab = 'WCSS')
+
+# Applying k-means to the mall dataset
+set.seed(29)
+kmeans <- kmeans(X, 5, iter.max = 300, nstart = 10)
+
+# Visualing the clustering
+library(cluster)
+clusplot(X,
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of Clients'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
