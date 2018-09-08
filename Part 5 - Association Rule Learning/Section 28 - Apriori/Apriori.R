a519896fd4d3c520bac2ca9d07f99069e35adf5e
KaanHGunay <kaangunay16@gmail.com>
2018-09-08 22:57:58 +0300

    apriori

---
+++ Part 5 - Association Rule Learning/Section 28 - Apriori/Apriori.R
@@ -0,0 +1,16 @@
+# Apriori
+
+# Data Proprocessing 
+library(arules)
+# dataset = read.csv('Market_Basket_Optimisation.csv', header = FALSE)
+dataset = read.transactions('Market_Basket_Optimisation.csv', sep = ',', rm.duplicates = TRUE)
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 15)
+
+# Training Apriori on the dataset
+rules = apriori(data = dataset,
+                parameter = list(support = 0.004, 
+                                 confidence = 0.2))
+
+# Visualing the result
+inspect(sort(rules, by = 'lift')[1:10])
